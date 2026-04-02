10ca922b1722df695add6d15332ee6f1839cd096
SuPythony <sumanyu.code@gmail.com>
2026-04-02 13:10:13 +0000

    q4

--- q4/q4.c
+++ q4/q4.c
@@ -5,14 +5,14 @@
 int main() {
     char op[6];
     int a, b;
-    while (scanf("%s %d %d", op, &a, &b) == 1){
-	char libname[14] = "./lib";
+    while (scanf("%s %d %d", op, &a, &b)!=EOF){
+	char libname[20] = "./lib";
 	strcat(libname, op);
 	strcat(libname, ".so");
 	void *lib = dlopen(libname, RTLD_NOW);
 	int (*f)(int,int) = dlsym(lib, op);
 	printf("%d\n", f(a,b));
-	dlclose(lib);	
+	dlclose(lib);
     }
     return 0;
 }
