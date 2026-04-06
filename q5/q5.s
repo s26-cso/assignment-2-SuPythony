92cf280130c31ff99e4e4def7db7b0408f08b68b
SuPythony <sumanyu.code@gmail.com>
2026-04-06 18:48:55 +0000

    do without storing string

--- q5/q5.s
+++ q5/q5.s
@@ -8,76 +8,125 @@ no: .string "No\n"
 .text
 .globl main
 
-main:
+read_forw:
+#fp in a0
 addi sp, sp, -16
 sd ra, 0(sp)
-sd s1, 8(sp)
+call fgetc
+ld ra, 0(sp)
+addi sp, sp, 16
+ret
+
+read_rev:
+#fp in a0
+addi sp, sp, -32
+sd ra, 0(sp)
+sd s0, 8(sp)
+sd s1, 16(sp)
+
+mv s0, a0
+li a1, -1
+li a2, 1 #SEEK_CUR
+call fseek
+
+mv a0, s0
+call fgetc
+mv s1, a0
+
+mv a0, s0
+li a1, -1
+li a2, 1
+call fseek
+
+mv a0, s1
+
+ld s1, 16(sp)
+ld s0, 8(sp)
+ld ra, 0(sp)
+addi sp, sp, 32
+ret
+
+is_done:
+#forw fp in a0
+#rev fp in a1
+addi sp, sp, -32
+sd ra, 0(sp)
+sd s0, 8(sp)
+sd s1, 16(sp)
+
+mv s0, a1
+call ftell
+mv s1, a0
+mv a0, s0
+call ftell
+mv s0, a0
+addi s1, s1, 1
+li a0, 1
+blt s0, s1, is_done_end
+li a0, 0
+
+is_done_end:
+ld s1, 16(sp)
+ld s0, 8(sp)
+ld ra, 0(sp)
+addi sp, sp, 32
+ret
+
+main:
+addi sp, sp, -48
+sd ra, 0(sp)
+sd s0, 8(sp)
+sd s1, 16(sp)
+sd s2, 24(sp)
+sd s3, 32(sp)
+
 la a0, fname
 la a1, mode
 call fopen
-mv t0, a0 #file pointer
-li a0, 0
-call malloc
-mv t1, a0 #string pointer
-mv t2, zero #length
-addi s1, zero, 1 #cur max string lengthj
+mv s0, a0 #front file pointer
+la a0, fname
+la a1, mode
+call fopen
+mv s1, a0 #reverse file pointer
+mv a0, s1
+li a1, -1 #assuming \n at end
+li a2, 2 #SEEK_END
+call fseek #go to end
 
-#using realloc with doubling when full size reached
-#amortized O(1) complexity
 loop:
-mv a0, t0
-call fgetc
-blt a0, zero, end
-mv t3, a0 #cur char
-blt t2, s1, put #within array length
-mv a0, t1
-slli s1, s1, 2 #multiple max size by 2
+mv a0, s0
 mv a1, s1
-call realloc
-mv t1, a0
-put:
-add t1, t1, t2
-sb t3, 0(t1)
-sub t1, t1, t2
-addi t2, t2, 1
-jal zero, loop
-end:
-li t4, 10
-bne t3, t4, continue #is last char not \n
-addi t2, t2, -1 #if \n, decr length by 1
+call is_done
+beq a0, zero, not_done
+jal zero, print_yes
+not_done:
+mv a0, s0
+call read_forw
+mv s2, a0
+mv a0, s1
+call read_rev
+mv s3, a0
+beq s2, s3, loop
+jal zero, print_no
 
-continue:
-#palindrome checking
-li t4, 1 #t4 stores ans
-mv t6, t2 #t6 stores right pointer
-addi t6, t6, -1
-li t5, 0 #t5 stores left pointer
-loop2:
-add t1, t1, t5
-lb t2, 0(t1) #t2 stores char at left pointer
-sub t1, t1, t5
-add t1, t1, t6
-lb t3, 0(t1) #t3 stores char at right pointer
-sub t1, t1, t6
-bne t2, t3, notpal
-addi t5, t5, 1
-addi t6, t6, -1
-bge t5, t6, end2 #if left pointer >= right pointer, then done
-beq zero, zero, loop2
-notpal:
-li t4, 0
-end2:
-beq t4, zero, nopa
+print_yes:
 la a0, yes
 call printf
-jal zero, fin
-nopa:
+jal zero, main_end
+
+print_no:
 la a0, no
 call printf
-fin:
-mv a0, t0
+
+main_end:
+mv a0, s0
 call fclose
+mv a0, s1
+call fclose
+ld s3, 32(sp)
+ld s2, 24(sp)
+ld s1, 16(sp)
+ld s0, 8(sp)
 ld ra, 0(sp)
-ld s1, 8(sp)
-addi sp, sp, 16
+addi sp, sp, 48
 ret
